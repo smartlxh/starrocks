17ecccdeec9fde02edb09c5379474ac9df5ad652
lixianhai.lxh <lixianhai.lxh@alibaba-inc.com>
2023-10-26 10:45:04 +0800

    [Backport][Enhancement] Make scan metrics not duplicated for tablet internal parallel (#33579)
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/storage/rowset/rowset_options.h
+++ be/src/storage/rowset/rowset_options.h
@@ -38,11 +38,11 @@ class TabletSchema;
 class ColumnPredicate;
 class DeletePredicates;
 struct RowidRangeOption;
-struct ShortKeyRangeOption;
+struct ShortKeyRangesOption;
 
 class RowsetReadOptions {
     using RowidRangeOptionPtr = std::shared_ptr<RowidRangeOption>;
-    using ShortKeyRangeOptionPtr = std::shared_ptr<ShortKeyRangeOption>;
+    using ShortKeyRangesOptionPtr = std::shared_ptr<ShortKeyRangesOption>;
     using PredicateList = std::vector<const ColumnPredicate*>;
 
 public:
@@ -75,7 +75,7 @@ public:
     const std::unordered_set<uint32_t>* unused_output_column_ids = nullptr;
 
     RowidRangeOptionPtr rowid_range_option = nullptr;
-    std::vector<ShortKeyRangeOptionPtr> short_key_ranges;
+    ShortKeyRangesOptionPtr short_key_ranges_option = nullptr;
 
     OlapRuntimeScanRangePruner runtime_range_pruner;
 
