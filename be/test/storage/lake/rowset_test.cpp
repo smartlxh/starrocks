a7075f4fe61cc38de0a84f67d81288593ab4a0a3
smartlxh <smartlxh@gmail.com>
2024-02-29 14:32:05 +0800

    add be ut
    Signed-off-by: smartlxh <smartlxh@gmail.com>
    Signed-off-by: lixianhai <lixianhai.lxh@alibaba-inc.com>

--- be/test/storage/lake/rowset_test.cpp
+++ be/test/storage/lake/rowset_test.cpp
@@ -106,7 +106,7 @@ public:
     }
 
 protected:
-    constexpr static const char* const kTestDirectory = "test_lake_rowset";
+    constexpr static const char* const kTestDirectory = "test_lake_tablet";
 
     std::shared_ptr<TabletMetadata> _tablet_metadata;
     std::shared_ptr<TabletSchema> _tablet_schema;
