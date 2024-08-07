811444f6f2b7857690559f361fe2d22ca5ccd0f0
yuzhou.zk <yuzhou.zk@alibaba-inc.com>
2024-08-07 18:54:24 +0800

    [Backport][BugFix] Fix plan and alter concurrent issue (backport #49355) (#49366)
    Link: https://code.alibaba-inc.com/soe/starrocks/codereview/17840376
    * [Backport][BugFix] Fix plan and alter concurrent issue (backport #49355) (#49366)
    
    Signed-off-by: sevev <qiangzh95@gmail.com>
    Co-authored-by: zhangqiang <qiangzh95@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
@@ -2608,6 +2608,7 @@ public class SchemaChangeHandler extends AlterHandler {
 
             // for now table's state can only be NORMAL
             Preconditions.checkState(olapTable.getState() == OlapTableState.NORMAL, olapTable.getState().name());
+            olapTable.setState(OlapTableState.UPDATING_META);
             SchemaChangeJobV2 schemaChangeJob = new SchemaChangeJobV2(jobId, db.getId(), olapTable.getId(),
                                                                       olapTable.getName(), 1000);
             // update base index schema
@@ -2692,6 +2693,7 @@ public class SchemaChangeHandler extends AlterHandler {
             LOG.info("finished modify table's add or drop column(field). table: {}, is replay: {}", olapTable.getName(),
                     isReplay);
         } finally {
+            olapTable.setState(OlapTableState.NORMAL);
             db.writeUnlock();
         }
     }
