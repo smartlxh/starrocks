a568e2da00e6c9115b79fc66b27468a1aaf2aa6b
lixianhai <lixianhai.lxh@alibaba-inc.com>
2024-10-16 19:44:17 +0800

    add log
    Signed-off-by: smartlxh <smartlxh@gmail.com>

--- be/src/io/jindo_input_stream.cpp
+++ be/src/io/jindo_input_stream.cpp
@@ -138,14 +138,14 @@ StatusOr<std::unique_ptr<NumericStatistics>> JindoInputStream::get_numeric_stati
     //auto uuid_pattern = fmt::format(".*{}.*", _stream_uuid);
     LOG(INFO) << "uuid_pattern: " << _stream_uuid;
     jdo_setOption(_jindo_client->option, JDO_METRICS_FILTER_OPTION_LABEL_REGREX, _stream_uuid.c_str());
-    char* result_ptr = jdo_dumpMetrics(jdo_ctx, 2, 0, _jindo_client->option);
+    result_ptr = jdo_dumpMetrics(jdo_ctx, 2, 0, _jindo_client->option);
     if (result_ptr) {
         auto metrics = std::make_shared<std::string>(result_ptr);
         LOG(INFO) << "metrics dynamic label: " << (*metrics);
         free(result_ptr);
     }
 
-    char* result_ptr = jdo_dumpMetrics(jdo_ctx, 5, 0, _jindo_client->option);
+    result_ptr = jdo_dumpMetrics(jdo_ctx, 5, 0, _jindo_client->option);
     if (result_ptr) {
         auto metrics = std::make_shared<std::string>(result_ptr);
         LOG(INFO) << "metrics all label: " << (*metrics);
