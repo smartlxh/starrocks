c2c4e74a5731862514858fc9411c74bb1fe614bc
lixianhai <lixianhai.lxh@alibaba-inc.com>
2024-10-16 19:34:47 +0800

    add log
    Signed-off-by: smartlxh <smartlxh@gmail.com>

--- be/src/io/jindo_input_stream.cpp
+++ be/src/io/jindo_input_stream.cpp
@@ -128,14 +128,27 @@ StatusOr<std::unique_ptr<NumericStatistics>> JindoInputStream::get_numeric_stati
        };*/
     // result_type: 0 indicates the result is a json string, others indicates it is prometheus string
     // here 5 means ALL, 0 means json string
-
-    auto uuid_pattern = fmt::format(".*{}.*", _stream_uuid);
-    LOG(INFO) << "uuid_pattern: " << uuid_pattern;
-    jdo_setOption(_jindo_client->option, JDO_METRICS_FILTER_OPTION_LABEL_REGREX, uuid_pattern.c_str());
     char* result_ptr = jdo_dumpMetrics(jdo_ctx, 5, 0, _jindo_client->option);
     if (result_ptr) {
         auto metrics = std::make_shared<std::string>(result_ptr);
-        LOG(INFO) << "metrics label: " << (*metrics);
+        LOG(INFO) << "metrics all: " << (*metrics);
+        free(result_ptr);
+    }
+
+    //auto uuid_pattern = fmt::format(".*{}.*", _stream_uuid);
+    LOG(INFO) << "uuid_pattern: " << _stream_uuid;
+    jdo_setOption(_jindo_client->option, JDO_METRICS_FILTER_OPTION_LABEL_REGREX, _stream_uuid.c_str());
+    char* result_ptr = jdo_dumpMetrics(jdo_ctx, 2, 0, _jindo_client->option);
+    if (result_ptr) {
+        auto metrics = std::make_shared<std::string>(result_ptr);
+        LOG(INFO) << "metrics dynamic label: " << (*metrics);
+        free(result_ptr);
+    }
+
+    char* result_ptr = jdo_dumpMetrics(jdo_ctx, 5, 0, _jindo_client->option);
+    if (result_ptr) {
+        auto metrics = std::make_shared<std::string>(result_ptr);
+        LOG(INFO) << "metrics all label: " << (*metrics);
         free(result_ptr);
     }
 
