df071c1e0625cbad05d1bd32f5db370e63d57c1c
smartlxh <smartlxh@gmail.com>
2025-10-30 20:57:25 +0800

    support enable_starlet_peer_cache
    Signed-off-by: smartlxh <smartlxh@gmail.com>
    Signed-off-by: lixianhai <lixianhai.lxh@alibaba-inc.com>

--- be/src/service/staros_worker.cpp
+++ be/src/service/staros_worker.cpp
@@ -502,6 +502,8 @@ void init_staros_worker(const std::shared_ptr<starcache::StarCache>& star_cache)
     fslib::FLAGS_star_cache_disk_size_percent = config::starlet_star_cache_disk_size_percent;
     fslib::FLAGS_star_cache_disk_size_bytes = config::starlet_star_cache_disk_size_bytes;
     fslib::FLAGS_star_cache_block_size_bytes = config::starlet_star_cache_block_size_bytes;
+    
+    fslib::FLAGS_enable_peer_cache = config::starlet_enable_peer_cache;
 
     staros::starlet::StarletConfig starlet_config;
     starlet_config.rpc_port = config::starlet_port;
